c180c592c6fc942e7c4844559f2714abd70c8f98
Jacob <jacob4evans434@gmail.com>
2021-02-12 11:48:29 +0000

    added import error catch

--- crack.py
+++ crack.py
@@ -1,12 +1,20 @@
-import argparse as ap
-from datetime import datetime
-import hashlib
+try:
+    import argparse as ap
+    from datetime import datetime
+    import hashlib
+    import multiprocessing # ? potetnially use this for multiple hash cracking at once
+except ImportError:
+    print("Install required modules listed in README.md or upgrade python.")
+
+# TODO: Add suport for multiple hashes in hash file | add optional param for hash type
 
 parser = ap.ArgumentParser(description="A simple password cracker", epilog="After inputing both arguments then run")
 
 parser.add_argument('hashfile', help="Text file containing hash to be cracked.")
 
 parser.add_argument('wordlist', help="Path to the desired wordlist")
+
+# ? implement optional param for hash type here
 
 args = parser.parse_args()
 
@@ -32,7 +40,7 @@ def crackpassword(hash, wordlist):
         hashedline = hashlib.md5(line.encode()).hexdigest()
 
         if str(hashedline) == str(hash): # ! if the hashes are the same print the password
-            print("password is" + line + "finished on " + getdate + "at " + gettime) # prints password date and time 
+            print("password is" + line + "finished on " + getdate + " at " + gettime) # ! prints password date and time 
 
     return ""
 
@@ -40,4 +48,6 @@ def crackpassword(hash, wordlist):
 
 if __name__ == '__main__': # ! runs this when program stars
     crackpassword(args.hashfile, args.wordlist)
+
+# ? Basic usage: python3 crack.py testingtools/hash.txt testingtools/wordlist.txt
 
